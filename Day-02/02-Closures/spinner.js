d128737e5f630b5774fd1f4dab117a1bd85f41c7
Magesh <tkmagesh77@gmail.com>
2016-06-24 12:51:06 +0530

    spinner - problem statement

---
+++ Day-02/02-Closures/spinner.js
@@ -0,0 +1,15 @@
+create a 'spinner' object
+var spinner = ......
+
+the spinner object should exhibit the following:
+
+spinner.up(); //=> 1
+spinner.up(); //=> 2
+spinner.up(); //=> 3
+spinner.up(); //=> 4
+
+spinner.down(); => 3
+spinner.down(); => 2
+spinner.down(); => 1
+spinner.down(); => 0
+spinner.down(); => -1
