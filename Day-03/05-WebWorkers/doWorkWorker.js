326d310733372bc78a8b386b280856ab8ec86ffe
Magesh <tkmagesh77@gmail.com>
2016-06-25 15:28:08 +0530

    Server sent events

--- Day-03/05-WebWorkers/doWorkWorker.js
+++ Day-03/05-WebWorkers/doWorkWorker.js
@@ -1,15 +1,24 @@
 function doWork(){
-	for(var i=0; i<10000; i++)
+	for(var i=0; i<10000; i++){
 		for(var j=0; j<10000; j++)
 			for(var k = 0; k<100; k++){
 				
 			}
+
+		if (i % 100 === 0){
+			var percentCompleted = (i / 10000) * 100;
+			self.postMessage({
+				type : 'progress',
+				percentCompleted : percentCompleted
+			})
+		}
+	}
 }
 
 self.addEventListener("message", function(evtArg){
 	if (evtArg.data === 'start'){
 		doWork();
-		self.postMessage("done");
+		self.postMessage({type : "done"});
 	} else {
 		console.log('unknown message received', evtArg);
 	}
